c86d1e3e7bf83deff2b4c8ba5b1c8c46a6c036a0
João Pedro Maciel <joaopedromaciel007@gmail.com>
2024-04-26 11:43:58 -0400

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
+
+// Middleware para habilitar o CORS
+app.use((req, res: Response, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(router);
 app.use(cors());
 
